438035c4c390a9d521af88a2ca2ddf6b9d1f92eb
k1mlka <k1mlka@outlook.com>
2023-04-01 16:55:51 +0900

    feat: Mesagebox topmost

--- ErogeHelper/MessageBox.cs
+++ ErogeHelper/MessageBox.cs
@@ -4,8 +4,11 @@ namespace ErogeHelper;
 
 internal class MessageBox
 {
-    public static void Show(string text, string title = "ErogeHelper") => _ = MessageBox_(IntPtr.Zero, text, title, 0);
+    public static void Show(string text, string title = "ErogeHelper") => _ = MessageBox_(IntPtr.Zero, text, title, MB_TOPMOST);
 
     [DllImport("user32.dll", EntryPoint = "MessageBoxA", ExactSpelling = true, CharSet = CharSet.Ansi)]
     private static extern int MessageBox_(IntPtr hWnd, string lpText, string lpCaption, int uType);
+
+    const int MB_TOPMOST = 0x00040000;
+    // https://stackoverflow.com/a/48003123
 }
